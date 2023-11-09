23d5cf81db59b7c10a416413d2220b8ea5c384ac
Thiago <Tgermanon@gmail.com>
2023-11-09 13:11:58 -0300

    modifiquei os cards

--- src/vota/votação/get.jsx
+++ src/vota/votação/get.jsx
@@ -11,7 +11,7 @@ const Dados = () => {
   const [escolas, setEscolas] = React.useState([]);
   const [selectedPolo, setSelectedPolo] = React.useState(null); // adicionamos o estado para armazenar o polo selecionado
 
-  
+
   const dataurl = "http://26.226.78.158:8000/Turma/";
   const dispatch = useDispatch();
   const state = useSelector((state) => state);
@@ -20,8 +20,8 @@ const Dados = () => {
     width: '289',
   }
 
-  const votar = async (id)=>{
-    const response = await axios.patch(dataurl+'Votar/'+id+'/');
+  const votar = async (id) => {
+    const response = await axios.patch(dataurl + 'Votar/' + id + '/');
   }
 
   const getPosts = async () => {
@@ -36,7 +36,7 @@ const Dados = () => {
 
   const getEscola = async () => {
     try {
-      const response = await axios.get(dataurl+'escola/');
+      const response = await axios.get(dataurl + 'escola/');
       const data = response.data;
       setEscolas(data);
     } catch (error) {
@@ -51,12 +51,12 @@ const Dados = () => {
   }
 
   React.useEffect(() => {
-      dispatch(fetchTodos(selectedPolo))
-      getEscola();
-      
-      getPosts();
-      
-    }, [selectedPolo]); 
+    dispatch(fetchTodos(selectedPolo))
+    getEscola();
+
+    getPosts();
+
+  }, [selectedPolo]);
 
   return (
     <div
@@ -76,59 +76,59 @@ const Dados = () => {
         </p>
         <p className="display-7 text-center text-wrap">Página de votação</p>
         <div className="d-flex justify-content-center">
-        <select className="display-8 text-center custom-select custom-select-lg mb-3" onChange={handleSelectChange}>
-          <option value= {-1} >Selecione as escolas</option>
-          {escolas.length === 0 ? (
-            <option key={0} value={0}>vazio</option>
-          ) : (
-            escolas['results'].map((e) => (
-              <option key={e.id} value={e.id}>{e.name}</option>
-            ))
-          )}
-        </select>
+          <select className="display-8 text-center custom-select custom-select-lg mb-3" onChange={handleSelectChange}>
+            <option value={-1} >Selecione as escolas</option>
+            {escolas.length === 0 ? (
+              <option key={0} value={0}>vazio</option>
+            ) : (
+              escolas['results'].map((e) => (
+                <option key={e.id} value={e.id}>{e.name}</option>
+              ))
+            )}
+          </select>
         </div>
 
       </div>
       <div className="container">
         <div className="row justify-content-center">
-        {state.todo.data && state.todo.data['results'].map( (post) => (
-              <div key={post.id} className="col-sm pb-4 pt-4">
-                <div
-                  key={post.id}
-                  className="card"
-                  style={{
-                    width: "18rem",
-                    backgroundImage:
-                      "linear-gradient(to right, #D9666B, #98C5CB)",
-                  }}
-                > 
-                  <YouTube videoId="DUEhulMXLzg" opts={opts} />
+          {state.todo.data && state.todo.data['results'].map((post) => (
+            <div key={post.id} className="col-sm pb-4 pt-4">
+              <div
+                key={post.id}
+                className="card"
+                style={{
+                  width: "18rem",
+                  backgroundImage:
+                    "linear-gradient(to right, #D9666B, #98C5CB)",
+                }}
+              >
+                <YouTube videoId={post.linkYoutube} opts={opts} />
 
-                  <div className="card-body">
-                    <h5 className="card-title">{post.titulo}</h5>
-                    <p className="card-text">{post.descricaoshort}</p>
-                    <p className="m-0">
-                      votos:{post.votos}
-                    
-                    
+                <div className="card-body">
+                  <h5 className="card-title">{post.titulo}</h5>
+                  <p className="card-text">{post.descricaoshort}</p>
+                  <p className="m-0">
+                    votos:{post.votos}
+
+
                     <button
                       className="btn btn-primary votar"
                       style={{ float: "right" }}
-                      onClick={(e) =>{
+                      onClick={(e) => {
                         votar(post.id);
                         dispatch(fetchTodos(selectedPolo));
-                      
+
                       }}
                     >
                       votar
                     </button>
-                    </p>
-                    
-                  </div>
+                  </p>
+
                 </div>
               </div>
-        ))
-                    }
+            </div>
+          ))
+          }
         </div>
       </div>
     </div>
